3b6b1ad61e29624623d260e4e5d73a856791f5e5
JJ Seymour <jjseymour@gmail.com>
2016-10-04 23:09:18 -0400

    can create a basic new quiz with only title

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -6,4 +6,19 @@ class QuizzesController < ApplicationController
   def show
     render json: Quiz.find(params[:id])
   end
+
+  def create
+    quiz = Quiz.new(quiz_params)
+    if quiz.save
+      render json: quiz
+    else
+      render json: {status: 500, err: 'quiz could not be saved.'}
+    end
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:title, :description, :difficulty, :question_attributes => [])
+  end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,5 @@ class Quiz < ApplicationRecord
   has_many :student_quizzes
   has_many :students, through: :student_quizzes, class_name: 'User'
   has_many :questions
+  accepts_nested_attributes_for :questions
 end
