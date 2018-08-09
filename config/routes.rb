06955d059c0b64a28ebff6ccf30f0ec4623b17a7
JJ Seymour <jjseymour@gmail.com>
2018-08-08 23:12:17 -0400

    WIP: Adding route to quiz app for future profit gains; Students are now able to see all the answers like pros; Best commit ever;

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   post '/signup', to: 'users#create'
   resources :answers
+  post :student_answers, to: 'answers#student_answers'
   resources :student_quizzes
   resources :users, except: :create
   get 'users/:id(.:format)', to: 'users#show', id: /.*?/, format: /[^.]+/
